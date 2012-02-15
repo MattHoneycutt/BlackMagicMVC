90f7bac5b0604b612d735e6dadce7dd51c9f6e96
matthoneycutt <mbhoneycutt@gmail.com>
2012-02-14 23:21:27 -0600

    Check-pointing progress.

--- BlackMagic.WebForms/Global.asax.vb
+++ BlackMagic.WebForms/Global.asax.vb
@@ -1,34 +1,59 @@
 ﻿Imports System.Web.SessionState
+Imports System.Web.Routing
+Imports System.Web.Mvc
+Imports MvcContrib.PortableAreas
+Imports BlackMagic.Mvc
 
 Public Class Global_asax
-    Inherits System.Web.HttpApplication
+	Inherits System.Web.HttpApplication
 
-    Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
-        ' Fires when the application is started
-    End Sub
+	Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+		' Fires when the application is started
 
-    Sub Session_Start(ByVal sender As Object, ByVal e As EventArgs)
-        ' Fires when the session is started
-    End Sub
+		AreaRegistration.RegisterAllAreas()
+		RegisterRoutes(RouteTable.Routes)
+		BlackMagicRoutes.Register(RouteTable.Routes)
+		'PortableAreaRegistration.RegisterEmbeddedViewEngine()
 
-    Sub Application_BeginRequest(ByVal sender As Object, ByVal e As EventArgs)
-        ' Fires at the beginning of each request
-    End Sub
+	End Sub
 
-    Sub Application_AuthenticateRequest(ByVal sender As Object, ByVal e As EventArgs)
-        ' Fires upon attempting to authenticate the use
-    End Sub
+	Private Sub RegisterRoutes(routes As RouteCollection)
 
-    Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
-        ' Fires when an error occurs
-    End Sub
+		routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
 
-    Sub Session_End(ByVal sender As Object, ByVal e As EventArgs)
-        ' Fires when the session ends
-    End Sub
+		'' MapRoute takes the following parameters, in order:
+		'' (1) Route name
+		'' (2) URL with parameters
+		'' (3) Parameter defaults
+		'routes.MapRoute("Default",
+		'	"{controller}/{action}/{id}",
+		'	New With {.controller = "Home", .action = "Index", .id = UrlParameter.Optional}
+		')
 
-    Sub Application_End(ByVal sender As Object, ByVal e As EventArgs)
-        ' Fires when the application ends
-    End Sub
+	End Sub
+
+	Sub Session_Start(ByVal sender As Object, ByVal e As EventArgs)
+		' Fires when the session is started
+	End Sub
+
+	Sub Application_BeginRequest(ByVal sender As Object, ByVal e As EventArgs)
+		' Fires at the beginning of each request
+	End Sub
+
+	Sub Application_AuthenticateRequest(ByVal sender As Object, ByVal e As EventArgs)
+		' Fires upon attempting to authenticate the use
+	End Sub
+
+	Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
+		' Fires when an error occurs
+	End Sub
+
+	Sub Session_End(ByVal sender As Object, ByVal e As EventArgs)
+		' Fires when the session ends
+	End Sub
+
+	Sub Application_End(ByVal sender As Object, ByVal e As EventArgs)
+		' Fires when the application ends
+	End Sub
 
 End Class
